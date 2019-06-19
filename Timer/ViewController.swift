fef0a7e0de72092e1f462fedb51dd9acdeb39441
i5g2b <i5g2b@ugrad.cs.ubc.ca>
2019-06-19 16:00:21 -0700

    Initial Commit

---
+++ Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Reginald McDonald on 2019-06-19.
+//  Copyright © 2019 Reginald McDonald. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
